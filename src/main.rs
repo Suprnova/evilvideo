a6d5133201effa563fb850293dd6ff31c516414e
Suprnova <nova@suprnova.dev>
2025-02-07 11:44:43 -0500

    more args, radvideo binary handling

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,27 @@
 use std::{fs::File, path::PathBuf};
 
 use clap::{error::ErrorKind, CommandFactory, Parser, ValueEnum};
+use which::which;
 
+// TODO: Support multiple inputs and outputs,
+// add field for custom Binkc
 #[derive(Parser)]
 #[command(version, about)]
 struct Cli {
     #[arg(short, long = "input")]
     /// file to convert
-    infile: std::path::PathBuf,
+    infile: PathBuf,
 
     /// target game
     #[arg(short, long, value_enum)]
     format: GameFormat,
+
+    /// automatically overwrite output file
+    #[arg(short = 'y', long, default_value_t = false)]
+    overwrite: bool,
+
+    #[arg(short, long)]
+    radvideo_path: Option<PathBuf>,
 
     /// file to output
     outfile: Option<PathBuf>,
@@ -28,19 +38,61 @@ enum GameFormat {
     /// The Incredibles
     Incredibles,
     /// The Incredibles: Rise of the Underminer
-    ROTU
+    ROTU,
+}
+
+impl GameFormat {
+    fn _get_resolution(&self) -> (i32, i32) {
+        match *self {
+            Self::N100F | Self::Battle => (640, 480),
+            Self::Movie | Self::ROTU => (512, 480),
+            Self::Incredibles => (512, 448),
+        }
+    }
 }
 
 fn main() {
     let args = Cli::parse();
-    let _infile = File::open(&args.infile)
-        .unwrap_or_else(|_| {
-            let mut cmd = Cli::command();
-            cmd.error(
-                ErrorKind::Io,
-                format!("INFILE {} does not exist", args.infile.display())
-            )
-            .exit()
+
+    let _rad_path = &args.radvideo_path.unwrap_or_else(|| {
+        which("radvideo64.exe")
+            .unwrap_or_else(|_| {
+                let mut cmd = Cli::command();
+                cmd.error(
+                    ErrorKind::MissingRequiredArgument,
+                    "RADVideo doesn't appear to be installed, or it's not located in %PATH%\n \
+                please download RADVideo and configure your %PATH% to the RADVideo folder, \
+                or set the -r argument to the path of the RADVideo folder.",
+                )
+                .exit();
+            })
+            .parent()
+            .unwrap()
+            .to_path_buf()
+    });
+    which(_rad_path.join("radvideo64.exe")).unwrap_or_else(|_| {
+        let mut cmd = Cli::command();
+        cmd.error(
+            ErrorKind::InvalidValue,
+            format!(
+                "RADVideo doesn't appear to be installed in the provided directory {}\n\
+            Please ensure the provided directory contains the radvideo64 executable, or clear the \
+            -r argument to use your %PATH%, if configured correctly.",
+                &_rad_path.display()
+            ),
+        )
+        .exit();
     });
 
+    let _infile = File::open(&args.infile).unwrap_or_else(|_| {
+        let mut cmd = Cli::command();
+        cmd.error(
+            ErrorKind::Io,
+            format!("INFILE {} does not exist", args.infile.display()),
+        )
+        .exit()
+    });
+
+    // TODO: prompt to overwrite if flag isn't set
+    let _outfile = &args.outfile.unwrap_or(args.infile.with_extension("bik"));
 }
